3ebd5f495010bde5c1fc0d22256ef42d8f68730a
Luca Ciucci <LucaCiucci@users.noreply.github.com>
2020-02-09 19:49:21 +0100

    programmi arduino
    ho aggiunto dei programmi di test per arduino

---
+++ teensy_differenziale_sincronizzato/teensy_differenziale_sincronizzato.ino
@@ -0,0 +1,19 @@
+#include <ADC.h>
+
+const int readPin = A9;
+const int readPin2 = A3;
+
+const int chargePin = 15;
+const int dischargePin = 16;
+const int pulsePin = 13;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
