744009577df0ca0e5f301f1b849642e4166774bc
b.tomelleri <bernardo.tomelleri@gmail.com>
2020-02-14 00:59:38 +0100

    Sintassi Python per controllo array e file

--- teensy_differenziale_definitivo/control.py
+++ teensy_differenziale_definitivo/control.py
@@ -3,46 +3,36 @@ import pylab
 import numpy
 from matplotlib import pyplot as plt
 
-Directory = '../data_did/'
-FileName = 'dati_0.22_1.txt'
-Directory2 = '../data_elaborati/'
-FileName2 ='dati_0.22_1el.txt'
-Directory3 = '../tmp/'
-FileName3 = 'tmp0.22_1.txt'
+Dir = '../data_did/'
+fName = 'dati_0.22_1.txt'
+Dir2 = '../tmp/'
+fName2 = 'tmp0.22_1.txt'
+Dir3 = '../data_elaborati/'
+fName3 ='dati_0.22_1el.txt'
 
-
-x = []
-y = []
-
-f = open(Directory + FileName, 'r')
-f2 = open(Directory3 + FileName3, 'w')
-lines = f.readlines()
-for i in range(len(lines)):
-    if len(lines[i]) <= 14:
-        f2.write(lines[i])
-        f2.write("\n")
-f.close()
-f2.close()
-x, y = numpy.loadtxt(Directory3 + FileName3, unpack = True)
+with open(Dir + fName, 'r') as f, open(Dir2 + fName2, 'w') as f2:
+    lines = f.readlines()
+    for ln in lines:
+        if len(ln) <= 14:
+            f2.write(ln)
+            f2.write("\n")
+            
+x, y = numpy.loadtxt(Dir2 + fName2, unpack = True)
 plt.errorbar(x, y, linestyle = '', marker = '.')
 plt.show()
-
 
 t = []
 v = []
-for i in range(len(x)):
-    if(x[i]<4100):
-        t.append(x[i])
-        v.append(y[i])
+for value in x:
+    if(value < 4100):
+        t.append(value)
+        v.append(value)
 
 x = numpy.array(t)
 y = numpy.array(v)
-
 plt.errorbar(x, y, linestyle = '', marker = '.')
 plt.show()
 
-f3 = open(Directory2 + FileName2, 'w')
-for i in range(len(x)):
-    f3.write("%f    %f\n" % (x[i], y[i]))
-
-f3.close()
+with open(Dir3 + fName3, 'w',) as f3:
+    for i in range(len(x)):
+        f3.write("%f    %f\n" % (x[i], y[i]))
