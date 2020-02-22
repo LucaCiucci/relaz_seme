eaf9c8abf2544c4aa3145df36447bc511bb8b84e
Serena1999 <bruzzesi.serena@gmail.com>
2020-02-22 22:35:57 +0100

    carbacqua

--- Cartella_fit/call_exe.py
+++ Cartella_fit/call_exe.py
@@ -6,29 +6,26 @@ from load_data import *
 
 file2C = open('filetestoC/file2C.txt', 'w')
 
-file2C.write("#V    dV    I    dI")
+file2C.write("#V    errV    stdV    I    errI    stdI\n")
 
 for i in range(len(voltages)):
     for j in range(len(voltages[i])):
-        ##dovrebbero essere sei gli elementi ma ora non ricordo come si chiamano
-        ##dell variabili... da chiedere a Luca
-        file2C.write("%f    %f    %f    %f\n" \
-                     %(voltages[i][j], voltageStds[i][j],\
-                       currents[i][j], currentStds[i][j]))
+        file2C.write("%.20f    %.20f    %.20f    %.20f    %.20f    %.20f\n" \
+                     %(voltages[i][j], voltageErrs[i][j], voltageStds[i][j],\
+                       currents[i][j], currentErrs[i][j],  currentStds[i][j]))
     file2C.write("#================================")
+
 file2C.close()
 
-os.system('test_per_Serena')
+import subprocess
 
-time.sleep(5)
-##serve per assicurarsi che il filefromC.txt sia stato creato prima di andare
-##avanti
-while(os.path.exists("filetestoC/filefromC.txt") == False):
-    time.sleep(5)
+subprocess.call('test_per_Serena')
 
 ##serve per assicurarsi che l'eseguibile abbia finito a scrivere su file
 time.sleep(10) ##da modificare una volta che si ha il programma in C++
 
 #il file di testo finale sarà coi numeri giusti una volta che si è scritto il
 #file in C++. Questo è di prova
-V, dV, I, dI = pylab.loadtxt("filetestoC/filefromC.txt", unpack = True)
+voltages, voltageErrs, voltageStds, currents,\
+          currentErrs, currentStds\
+          = pylab.loadtxt("filetestoC/filefromC.txt", unpack = True)
