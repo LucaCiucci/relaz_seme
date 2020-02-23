51301171031a53948b87fab2ad7346e1eddd55ce
Luca Ciucci <LucaCiucci@users.noreply.github.com>
2020-02-24 00:27:10 +0100

    /

--- Cartella_fit/filter_src/functions.hpp
+++ Cartella_fit/filter_src/functions.hpp
@@ -10,36 +10,53 @@
 #include <chrono>
 #include <thread>
 #include <random>
+#include <tuple>
 
 extern double m_2pi;
 extern double normFactor;
 
+
+// ================================
+//            CLASSI
+// ================================
+
+// rappresenta una riga del file
 class Row
 {
 public:
 	double V = 0, errV = 0, stdV = 0, I = 0, errI = 0, stdI = 0;
 };
 
-
-
 //using Row = std::array<double, 6>;// una riga è formata da 6 numeri e una ciambella
-using RunData = std::vector<Row>;// rappresenta un blocco di dati
+using RunData = std::vector<Row>;// rappresenta un blocco di dati (1 run)
 using RunSet = std::vector<RunData>;// rappresetna un insieme di run
 
 RunSet readFile(std::string fileName);
 
+// stampa una riga
 std::ostream& operator<<(std::ostream& stream, const Row& row);
+
+// stampa un blocco
 std::ostream& operator<<(std::ostream& stream, const RunData& data);
+
+// stampa un insieme di blocchi separati da "#=======..."
 std::ostream& operator<<(std::ostream& stream, const RunSet& set);
 
+// ================================
+//            FUNZIONI
+// ================================
+
+// quadrato di un DOUBLE (non mettere in template!)
 inline double sqr(double x) { return x * x; }
 
-inline double gaussian(double x, double mx, double sx)
+// funzione gaussiana centrata in zero
+inline double gaussian(double x, double sx)
 {
-	return normFactor * exp(-sqr((x - mx)/sx) * 0.5) / sx;
+	return normFactor * exp(-sqr(x/sx) * 0.5) / sx;
 }
 
-std::tuple<double, double> meanSigma(double x, const RunData& runData);
+// ritorna media, varianza y, marianza su media
+std::tuple<double, double, double> meanSigma(double x, const RunData& runData);
 
 
 #endif // !FUNCTIONS_H
